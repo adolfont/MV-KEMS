45b2625e72919138c9e21e8e76913505f3d0a7c1
Adolfo Neto <adolfo.usp@gmail.com>
2010-04-15 11:45:31 -0300

    First classes implemented

---
+++ TDD_experiments/FLISOL_2010/src/mvkems/formulas/AtomicFormula.java
@@ -0,0 +1,11 @@
+package mvkems.formulas;
+
+public class AtomicFormula implements Formula {
+	
+	private int id;
+
+	public AtomicFormula(int id) {
+		this.id=id;
+	}
+
+}
---
+++ TDD_experiments/FLISOL_2010/src/mvkems/formulas/Formula.java
@@ -0,0 +1,5 @@
+package mvkems.formulas;
+
+public interface Formula {
+
+}
