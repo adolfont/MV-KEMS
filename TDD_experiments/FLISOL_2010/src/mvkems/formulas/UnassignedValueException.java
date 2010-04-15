28a4e58cf04557fe90462c55cbea86d9dbda0816
Adolfo Neto <adolfo.usp@gmail.com>
2010-04-15 13:50:16 -0300

    getErrorWhenGettingValueForUnassignedAtomicFormula added

---
+++ TDD_experiments/FLISOL_2010/src/mvkems/formulas/UnassignedValueException.java
@@ -0,0 +1,10 @@
+package mvkems.formulas;
+
+public class UnassignedValueException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3153440755237640493L;
+
+}
